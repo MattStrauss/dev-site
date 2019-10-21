3e03eaa7ddc2db22a9e762ac58a1a366f2c516f3
570studio <strausstester@gmail.com>
2019-10-21 08:32:08 -0400

    Minor updates to styling

--- resources/js/Pages/Projects.vue
+++ resources/js/Pages/Projects.vue
@@ -1,12 +1,16 @@
 <template>
     <layout>
         <div class="bg-gray-600 p-6 inset-y-auto object-center">
-            <div class="max-w-sm rounded overflow-hidden bg-gray-200 mt-3 mb-3 shadow-lg mx-auto ">
+            <div class="text-center text-gray-100">
+                <h2 class="text-3xl font-hairline text-yellow-500">Projects</h2>
+                <div class="w-16 border-t-4 border-yellow-500 text-center ml-auto mr-auto mt-3 mb-3">  </div>
+            </div>
+            <div class="max-w-lg rounded overflow-hidden bg-gray-200 mt-3 mb-3 shadow-lg mx-auto border-gray-700 border-2">
                 <img class="w-full" src="/images/grocery.jpg" alt="Grocery Aisle">
                 <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2 text-yellow-600"><a href="https://swiftgrocerylist.com" class="hover:text-yellow-500">Swift Grocery List</a></div>
                     <p class="text-gray-700 text-base">
-                        A grocery list making web application built with Laravel and Vue.js. Includes recipes, aisle sorting and much more.
+                        A web application that helps you create a grocery shopping list fast. Built with Laravel and Vue.js. Includes recipes, drag-and-drop aisle sorting and much more.
                     </p>
                 </div>
                 <div class="px-6 py-4">
